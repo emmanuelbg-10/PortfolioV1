bba1b7c8e9bab0d6513f8f10099b5f22db3ecfec
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-02-20 05:10:38 +0000

    1

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,26 @@
 export default [
   {
-    "id": 1,
-    "title": "Proyecto Uno",
-    "description": "Descripción breve del Proyecto Uno.",
-    "repoUrl": "https://github.com/emmanuelbg-10/EstudioTema4Emmanuel",
-    "liveUrl": "https://portfolioemmanuelbg10.vercel.app",
-    "imageUrl": `https://api.urlbox.com/v1/fquNVgUgJTmaB3If/png?url=https%3A%2F%2Femmanuelbg10.vercel.app%2F&full_page_mode=native&width=960&height=540&full_page=true`
+    id: 1,
+    title: "Proyecto Uno",
+    description: "Descripción breve del Proyecto Uno.",
+    repoUrl: "https://github.com/emmanuelbg-10/EstudioTema4Emmanuel",
+    liveUrl: "https://portfolioemmanuelbg10.vercel.app",
+    imageUrl: `https://api.urlbox.com/v1/fquNVgUgJTmaB3If/png?url=https%3A%2F%2Femmanuelbg10.vercel.app%2F&full_page_mode=native&width=960&height=540&full_page=true&timestamp=${new Date().getTime()}`
   },
   {
-    "id": 2,
-    "title": "Proyecto Dos",
-    "description": "Descripción breve del Proyecto Dos.",
-    "repoUrl": "https://github.com/tuusuario/proyecto-dos",
-    "liveUrl": "https://proyecto-dos.vercel.app",
-    "imageUrl": "https://api.screenshotapi.net/screenshot?token=B5remtdeZEH6tZu21ReZ7GyBy&url=https://mi-proyecto.vercel.app"
+    id: 2,
+    title: "Proyecto Dos",
+    description: "Descripción breve del Proyecto Dos.",
+    repoUrl: "https://github.com/tuusuario/proyecto-dos",
+    liveUrl: "https://proyecto-dos.vercel.app",
+    imageUrl: `https://api.screenshotapi.net/screenshot?token=B5remtdeZEH6tZu21ReZ7GyBy&url=https://mi-proyecto.vercel.app&timestamp=${new Date().getTime()}`
   },
   {
-    "id": 3,
-    "title": "Proyecto Tres",
-    "description": "Descripción breve del Proyecto Tres.",
-    "repoUrl": "https://github.com/tuusuario/proyecto-tres",
-    "liveUrl": "https://proyecto-tres.vercel.app",
-    "imageUrl": "https://api.screenshotapi.net/screenshot?token=B5irmtdeZEH6tZu21ReZ7GyBy&url=https://mi-proyecto.vercel.app"
+    id: 3,
+    title: "Proyecto Tres",
+    description: "Descripción breve del Proyecto Tres.",
+    repoUrl: "https://github.com/tuusuario/proyecto-tres",
+    liveUrl: "https://proyecto-tres.vercel.app",
+    imageUrl: `https://api.screenshotapi.net/screenshot?token=B5irmtdeZEH6tZu21ReZ7GyBy&url=https://mi-proyecto.vercel.app&timestamp=${new Date().getTime()}`
   }
 ];
