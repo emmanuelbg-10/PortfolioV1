d395a8759a52d47e94cdfa3284f767fb0041a65d
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-02-20 20:09:08 +0000

    Refactor estilos y estructura de componentes: agrega archivos CSS globales, implementa un interruptor de tema y ajusta estilos en Projects.vue y NavBar.vue.

--- src/App.vue
+++ src/App.vue
@@ -9,57 +9,7 @@
 </template>
 
 <script setup>
+import './components/assets/App.css';
 import NavBar from "@/components/NavBar.vue";
 import ProjectCard from "./components/Projects.vue";
 </script>
-
-<style>
-/* Definición de variables y estilos globales */
-:root {
-  --primary-color: #007bff;
-  --secondary-color: #0056b3;
-  --background-color: #f5f5f5;
-  --card-background: #ffffff;
-  --text-color: #333;
-  --shadow: rgba(0, 0, 0, 0.1);
-  --shadow-hover: rgba(0, 0, 0, 0.2);
-  --border-radius: 12px;
-  --transition-speed: 0.3s;
-  --navbar-height: 60px;
-  /* Ajustar según la altura de la barra de navegación */
-}
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  font-family: 'Roboto', sans-serif;
-  background-color: var(--background-color);
-  color: var(--text-color);
-  line-height: 1.6;
-}
-
-a {
-  text-decoration: none;
-  color: var(--primary-color);
-}
-
-#app {
-  padding: 0;
-  /* Eliminar padding del contenedor #app */
-}
-
-.content {
-  margin-top: var(--navbar-height);
-  /* Añadir margen superior para el contenido */
-  padding: 1rem;
-}
-
-@media (max-width: 768px) {
-  .content {
-    padding: 0.5rem;
-  }
-}
-</style>
