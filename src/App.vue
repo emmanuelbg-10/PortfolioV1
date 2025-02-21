2d9b2c619ea541cfc17820bab3aaca2a7b918a7a
emmanuelbg-10 <emmanuelbgiraldo@gmail.com>
2025-02-21 06:39:43 +0000

    Agrega meta descripción en index.html y reestructura App.vue: introduce un encabezado con título y estilo centrado.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="app">
+  <header>
     <NavBar />
-    <main class="content">
-      <ProjectCard />
-      <!-- <router-view /> -->
-    </main>
-  </div>
+    <h1 class="header-title">Hola</h1>
+    <p></p>
+
+  </header>
+  <main class="content">
+    <ProjectCard />
+    <!-- <router-view /> -->
+  </main>
+
 </template>
 
 <script setup>
@@ -13,3 +17,13 @@ import './components/assets/App.css';
 import NavBar from "@/components/NavBar.vue";
 import ProjectCard from "./components/Projects.vue";
 </script>
+
+<style scoped>
+.header-title {
+  font-size: 2rem;
+  text-align: center;
+  margin-top: 40px;
+  padding-top: 20px;
+  /* Add padding to prevent NavBar overlap */
+}
+</style>
